bafd7787994b84895e41ec5227d6af4643e7d7bd
heyeshuang <yeshuanghe@gmail.com>
2015-12-02 21:44:06 +0800

    highlighted

--- js/application/components/LyricsTable/LyricsTable.jsx
+++ js/application/components/LyricsTable/LyricsTable.jsx
@@ -7,48 +7,55 @@ import {Table,TableHeader,TableHeaderColumn,TableBody,
   TableRow,TableRowColumn,TableFooter} from 'material-ui';
 import LyricsRow from './LyricsRow.jsx';
 import {LrcPre,lyricsDemo} from '../LrcPreprocess.js';
-  class LyricsTable extends Component {
-    _bind(...methods) {
-      methods.forEach( (method) => this[method] = this[method].bind(this) );
-    }
-    constructor(){
-      super();
-      this.state = {
-      };
-      this._bind('render');
-    }
-    render(){
-      return (
-        <Table
-          height='auto'
-          fixedHeader={false}
-          selectable={false}
-          multiSelectable={false}
-          onCellClick={this._onCellClick}
+class LyricsTable extends Component {
+  _bind(...methods) {
+    methods.forEach( (method) => this[method] = this[method].bind(this) );
+  }
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+    this._bind('render');
+  }
+  render(){
+    return (
+      <Table
+        height='auto'
+        fixedHeader={false}
+        selectable={false}
+        multiSelectable={false}
+        onCellClick={this._onCellClick}
         >
-          <TableHeader enableSelectAll={this.state.enableSelectAll}
-            adjustForCheckbox={false} displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn width='100px' tooltip='timestamp'>Time</TableHeaderColumn>
-              <TableHeaderColumn tooltip='lyrics row'>Lyrics</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            deselectOnClickaway={false}
-            showRowHover={true}
-            displayRowCheckbox={false}>
-            {LrcPre(lyricsDemo).map((value)=>{
-                return(
-                  <LyricsRow isSelected={false} time={value[0]}
-                  lyricsContent={value[1]} onCellClick={this._onCellClick}/>
-                )
-              })}
-          </TableBody>
-        </Table>
-      )}
-      _onCellClick(a,b){
-      }
-    }
+        <TableHeader enableSelectAll={this.state.enableSelectAll}
+          adjustForCheckbox={false} displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn width='100px' tooltip='timestamp'>Time</TableHeaderColumn>
+            <TableHeaderColumn tooltip='lyrics row'>Lyrics</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          deselectOnClickaway={false}
+          showRowHover={true}
+          displayRowCheckbox={false}>
+          {LrcPre(lyricsDemo).map((value,index)=>{
+            return(
+              <LyricsRow isSelected={index==this.props.selectedRow?true:false}
+                time={value[0]} key={value[0]}
+                lyricsContent={value[1]} onCellClick={this._onCellClick}/>
+            )
+          })}
+        </TableBody>
+      </Table>
+    )
+  }
+  _onCellClick(a,b){
+    console.log(a,b);
+      //TODO:修改此事件，现在不工作
+  }
+}
 
+LyricsTable.defaultProps={
+  selectedRow:0,
+}
 
-    export default LyricsTable;
+export default LyricsTable;
